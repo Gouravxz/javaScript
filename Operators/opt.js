7a416dabd03eb7a72321dc6aff806950401a05a6
Gourav <gouravxzx@gmail.com>
2026-09-12 11:08:09 +0530

    1st commmit

---
+++ Operators/opt.js
@@ -0,0 +1,52 @@
+// Arthemetic operators
+let a = 25;
+let b = 2;
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("a+b = ", a+b);
+console.log("a-b = ", a-b);
+console.log("a*b = ", a*b);
+console.log("a/b = ", a/b);
+// basic opt modulas and exponation
+// decremnet and incemret opt Unary opt
+// Unary opt are like a++ , a-- like this pre and post
+a++; console.log("a = ", a);
+// post decrement 
+++a; console.log("a = ", a);
+// pre decerement 
+a--; console.log("a = ", a);
+--a; console.log("a = ", a);
+// modulas 
+let x = 5;
+let y = 2;
+console.log("x = ", x);
+console.log("y = ", y);
+console.log("x % y = ", x % y);
+// now  exponation like 5^2 like this
+console.log("x ** y = ", x**y);
+// all opt complet today i learn.
+// practiceTask One Grocry Bill Calculator.
+let itemPrice = 45;
+let  qauntity = 3;
+let delveryFee =20;
+let cashGiven =200;
+// totoal price of item and qntity
+console.log("TotalPrice = ", itemPrice*qauntity);
+//final price with delvery fee.
+let tP = 135;
+console.log("finalprice = ", tP+delveryFee);
+// customer give me 200  and and tha final bill is 155 so give the chanage
+let finalBill = 155;
+console.log("change = ", cashGiven-finalBill);
+// modula chec qnt
+console.log("qnt rem =", qauntity%2);
+console.log("area =", 5**2);
+// qaunity low baecuaew item sell
+qauntity = qauntity - 3;
+console.log("qnt =", qauntity);
+// now qnt decremnt sipply add usw  for ex a++
+qauntity = qauntity + 10;
+// we can aslo use this short cut
+qauntity++;
+console.log("qnt =", qauntity);
+// i don't use diffent 20 d charge beacuse we sell all 3 item tpo a singal person se i only add tha delebry fee 20 not 60.
