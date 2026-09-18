0148d85b066f7ac91f9ab0c6d88a68df6ad49cc8
Gourav <Gouravxzx@gmail.com>
2026-09-18 12:47:47 +0530

    Refactor movie ticket pricing with user inputs
    Updated movie ticket pricing logic to include user input for movie type, ticket count, and cash given. Added calculations for total price, glass rent, discount, and change.

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 // details
 let age = prompt("enter ypur age");
-let movieType = "3D";
-let ticketCount = 2;
+let movieType = prompt("enter type 3d or 2d");
+let ticketCount = prompt("how many ticket you want");
 let hasStudentCard = true;
-let cashGiven =1000;
+let cashGiven = prompt("give cash");
+let glassRent = 30;
+let threeDprice = 300
+let twoDprice = 200;
 // age regitrications
 if (age >= 18)
 {
     console.log("you can watch");
 }
 else { console.log("you can't watch"); }
+// movie price
+if (movieType === "3D"){
+console.log("price =", 300);
+}
+else if (movieType === "2D")
+ { console.log("price =", 200);}
+else { console.log("movie type =", "invalid");
+}
+// tota price if someone choose 3d type
+total = threeDprice * ticketCount;
+console.log("total =",total);
+// add rent 30 glass rent
+total +=30;
+console.log("rent =", glassRent);
+// find disount 
+discount = total * 0.90;
+console.log("discount 10% =", discount);
+// paymrnt
+cashGiven >= total;
+change = cashGiven - total;
+console.log("Final price =", discount);
+console.log("Change =", change);
