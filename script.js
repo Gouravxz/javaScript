3765969b4d33ffa175b2b70dac912bb7862905f5
Gourav <Gouravxzx@gmail.com>
2026-09-18 12:06:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// details
+let age = prompt("enter ypur age");
+let movieType = "3D";
+let ticketCount = 2;
+let hasStudentCard = true;
+let cashGiven =1000;
+// age regitrications
+if (age >= 18)
+{
+    console.log("you can watch");
+}
+else { console.log("you can't watch"); }
